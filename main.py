fb7ac9f476b7f0946a1a57cc6071efbe59216df3
coledworld <cgnoreika@wpi.edu>
2022-02-07 10:46:59 -0500

    Basic tower validity logic

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 def assertTowerValid(tower):
-    pass
+    if tower[0][0] != "Door":
+        print("Invalid tower: Does not start with door.")
+        return False
+
+    if tower[len(tower) - 1][0] != "Lookout":
+        print("Invalid tower: Does not end with lookout.")
+        return False
+
+    for index, piece in enumerate(tower):
+        if piece[0] == "Door" and index != 0:
+            print("Invalid tower: Door found in invalid position.")
+            return False
+
+        if piece[0] == "Lookout" and index != (len(tower) - 1):
+            print("Invalid tower: Door found in invalid position.")
+            return False
+
+    return True
+
 
 if __name__ == "__main__":
-    pass
+    test_tower = [["Door", 1, 1, 1], ["Wall", 1, 1, 1], ["Lookout", 1, 1, 1]]
+    tower_is_valid = assertTowerValid(test_tower)
+    print(tower_is_valid)
+
+    test_tower = [["Lookout", 1, 1, 1], ["Wall", 1, 1, 1], ["Door", 1, 1, 1]]
+    tower_is_valid = assertTowerValid(test_tower)
+    print(tower_is_valid)
+
+    test_tower = [["Door", 1, 1, 1], ["Door", 1, 1, 1], ["Lookout", 1, 1, 1]]
+    tower_is_valid = assertTowerValid(test_tower)
+    print(tower_is_valid)
